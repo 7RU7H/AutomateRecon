61931a8f14f464298ed18e8867e110207138258a
truth <mkthun@protonmail.com>
2022-09-22 11:04:29 +0100

    extract-masscan found ports fixed - ping not so

--- exec-ping-ttl-enum.sh
+++ exec-ping-ttl-enum.sh
@@ -11,13 +11,14 @@ echo "Conducting Ping test against $1.."
 echo
 PINGOUT=$(ping -W 30 -c 1 $1) # check output if -Pn needed; if just exit - # TODO ttl for os detection
 wait
+ENDTIME=$(date)
 echo "Ping test complete against $1!"
 echo
 PINGRECV=$(echo $PINGOUT | cut -d ' ' -f 40)
 TTLFIELD=$(echo $PINGOUT | awk '{print $1}' | tr -d '[A-Za-z-\n]' | cut -c -2)
-if ["$PINGRECV" != "3"]
+if [ "$PINGRECV" != "1" ]
 then
-        echo "Ping test recieved $PINGRECV packets"
+        echo "Ping test on $STARTTIME recieved $PINGRECV packets by $ENDTIME"
         echo
 	echo "Ping test OS detection: $TTLFIELD"
 	echo
--- extract-masscan-found-ports.sh
+++ extract-masscan-found-ports.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 if [ "$#" -ne 2 ]; then
-	echo "Usage: $0 <project directory> <dash-delimited-ip>"
+	echo "Usage: $0 <dash-delimited-ip>"
 	exit
 fi
-
-LIST=(cat $1/masscan/$2-masscan.log | grep -v "#" | awk '{print $7}' | cut -d "/" -f1 | tr -s '\n' ', ')
+CWD=$(pwd)
+LIST=(cat $CWD/masscan/$1-masscan.log | grep -v "#" | awk '{print $7}' | cut -d "/" -f1 | tr -s '\n' ', ')
 DECOMMA=${LIST::-1}
 echo $DECOMMA
 exit
