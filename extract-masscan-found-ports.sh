002e372e6dd886f10b5db851114df0a7debe61ef
truth <mkthun@protonmail.com>
2022-09-23 09:30:37 +0100

    Fixed Extr masscan found ports

--- extract-masscan-found-ports.sh
+++ extract-masscan-found-ports.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 1 ]; then
 	echo "Usage: $0 <dash-delimited-ip>"
 	exit
 fi
 CWD=$(pwd)
-LIST=(cat $CWD/masscan/$1-masscan.log | grep -v "#" | awk '{print $7}' | cut -d "/" -f1 | tr -s '\n' ', ')
-DECOMMA=${LIST::-1}
+LIST=$(cat $CWD/masscan/$1-allports-masscan.log | grep -v "#" | awk '{print $7}' | cut -d "/" -f1 | tr -s '\n' ', ')
+LENLIST=${#LIST}-1
+DECOMMA=${LIST:0:LENLIST}
 echo $DECOMMA
 exit
-
