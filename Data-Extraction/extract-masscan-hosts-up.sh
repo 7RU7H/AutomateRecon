9138e62759565d7a2f8f0f3e54bac1c5eec3f2a3
truth <mkthun@protonmail.com>
2023-09-11 12:22:55 +0100

    More fixes

--- Data-Extraction/extract-masscan-hosts-up.sh
+++ Data-Extraction/extract-masscan-hosts-up.sh
@@ -3,10 +3,11 @@
 
 if [ "$#" -eq 0 ]; then
 	echo "Usage: $0 <Network Range>"
+	echo "$0 10.10.10.0/24"
 	exit
 fi
 
 dash_delimited_network_range=$(echo $1 | tr -s '.' '-' | sed 's#/#-cidr-#g')
 CWD=$(pwd)
-cat $CWD/masscan/$dash_delimited_network_range-masscan.log | awk '{print $4}' | grep -v 'Host' | sort -u > $CWD/masscan/masscan-found-ips.txt
+cat $CWD/masscan/$dash_delimited_network_range-masscan.log | grep 'Host' | awk '{print $4}' sort -u | tee -a  $CWD/hosts.txt
 exit
