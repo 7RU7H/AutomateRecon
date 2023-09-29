5b807026aea0b4ffa9acd8145037ec25c9fca0aa
truth <mkthun@protonmail.com>
2023-09-29 20:55:20 +0100

    Woops on the newlines of wingus and dingus's bash

--- mk-int-files.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-touch usernames.txt passwords.txt  
-echo "# Credentials\n\n| User | Password | Hash | Service | Machine |\n| --- | --- | --- | --- | --- | \n | x | x | x | x |" > credentials.md
-echo "\n# Machines\n\n| IPv4 | Hostname | Domain | Role |\n| --- | --- | --- | --- |\n\n Table is just guide; instead of rows create mindmaps of information\n" > machines.md
---
+++ mk-proj-intel-files.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+touch usernames.txt passwords.txt  
+echo "# Credentials
+
+| User | Password | Hash | Service | Machine |
+| --- | --- | --- | --- | --- |
+| x | x | x | x | x |" > credentials.md
+echo "# Machines
+
+| IPv4 | Hostname | Domain | Role |
+| --- | --- | --- | --- |
+
+Table is just guide; instead of rows create mindmaps of information" > machines.md
