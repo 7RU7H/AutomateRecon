7b3508580b85d8db6e851287a2893906c53fa2e9
truth <mkthun@protonmail.com>
2023-09-29 20:50:53 +0100

    Make intel files users, passwds,machines and creds

---
+++ mk-int-files.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+touch usernames.txt passwords.txt  
+echo "# Credentials\n\n| User | Password | Hash | Service | Machine |\n| --- | --- | --- | --- | --- | \n | x | x | x | x |" > credentials.md
+echo "\n# Machines\n\n| IPv4 | Hostname | Domain | Role |\n| --- | --- | --- | --- |\n\n Table is just guide; instead of rows create mindmaps of information\n" > machines.md
