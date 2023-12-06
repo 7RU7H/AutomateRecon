27a8fca5e61755e744d3ad9599ffdbf16110b5d2
truth <mkthun@protonmail.com>
2023-12-06 14:43:10 +0000

    Simple web app auto enumeration to prevent c+p@!#$

---
+++ exec-multitool-simple-web-app.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+	echo "Usage: $0 <URL>"
+	echo "Nuclei usage Offsec exam friendly with \`-etags expliot\` used"
+	exit
+fi
+
+URL=$1
+IP=$(echo $URL | awk -F/ '{print $3}')
+nuclei -u $URL -etags exploit -me nuclei
+wait
+gospider -d 0 -s "$URL" -c 5 -t 100 -d 5 --blacklist jpg,jpeg,gif,css,tif,tiff,png,ttf,woff,woff2,ico,pdf,svg,txt -o gospider
+wait
+nikto -h $IP -o nikto/http.txt 
+wait
+gobuster dir -u $URL -w /usr/share/seclists/Discovery/Web-Content/raft-small-directories-lowercase.txt -o www-root.gobuster
+wait
+echo ""
+echo "Done simple automated enumeration - remember maunal enumeration is always best"
+echo ""
+exit
