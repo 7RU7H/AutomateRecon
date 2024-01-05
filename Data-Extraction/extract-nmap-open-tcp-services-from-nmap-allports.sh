3aaac5bef3adedf746d8337844d64dc4ea3f7db8
none <mkthun@protonmail.com>
2024-01-05 23:11:29 +0000

    Add more me modern noting

--- Data-Extraction/extract-nmap-open-tcp-services-from-nmap-allports.sh
+++ Data-Extraction/extract-nmap-open-tcp-services-from-nmap-allports.sh
@@ -7,11 +7,20 @@ fi
 
 CWD=$(pwd)
 dash_delimited_ip=$(echo $1 | tr -s '.' '-')
-table_contents=$(cat $CWD/nmap/$dash_delimited_ip-Extensive.nmap | grep "/tcp " | awk '{print $1" "$2" | "$3" | "$4" "$5" "$6" "$7" "$8" "$9" "$10" "$11" "}')
+table_contents=$(cat $CWD/nmap/$dash_delimited_ip-Extensive-Found-Ports.nmap | grep "/tcp " | awk '{print $1" "$2" | "$3" | "$4" "$5" "$6" "$7" "$8" "$9" "$10" "$11" "}')
+list_for_notes=$(cat $CWD/nmap/$dash_delimited_ip-Extensive-Found-Ports.nmap | grep "/tcp " | awk -F/ '{print "- "$1}')
 echo "# Nmap Services found on host: $1
 
 Port & Protocol | Service | Version
 --- | --- | ---
 $table_contents
+
+$list_for_notes
+
+
 " > $CWD/nmap/found-services-for-$dash_delimited_ip.md
+
+
+
+
 exit
