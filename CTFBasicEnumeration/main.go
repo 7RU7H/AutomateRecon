78c5ec4292ba9ee8522b1cf7848f4c25b39185ef
truth <mkthun@protonmail.com>
2026-04-30 14:25:52 +0100

    Made functions and added ctx
    Added pingHost()
    Added CTRL+C support
    Started http request scaffolding

--- CTFBasicEnumeration/main.go
+++ CTFBasicEnumeration/main.go
@@ -1,16 +1,40 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"net/http"
 	"os"
 	"os/exec"
+	"os/signal"
 	"strings"
+	"syscall"
 	"time"
 )
 
-func testWebservers(ipAddress string) (testHTTP, testHTTPS bool, err error) {
+// Ping host to get TTL data to detirmine OS type
+func pingHost(ipAddress string) (bool, error) {
+	pingHostThrice := exec.Command("ping", "-c 3", ipAddress)
+	_, err := pingHostThrice.StdoutPipe()
+	if err != nil {
+		panic(err)
+	}
+	if err := pingHostThrice.Start(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error:", err)
+		fmt.Fprintf(os.Stdout, "Unable to execute `%s %s`\n", "ping -c 3 ", ipAddress)
+		return false, err
+	}
+	if err := pingHostThrice.Wait(); err != nil {
+		fmt.Fprintln(os.Stderr, "Error:", err)
+		fmt.Fprintf(os.Stdout, "Unable to complete execution of `%s %s`\n", "ping -c 3 ", ipAddress)
+		return false, err
+	}
 
+	time.Sleep(1 * time.Second)
+	fmt.Fprintf(os.Stdout, "Completed attempts to ping host, additional reminder to screenshot ping.png for notes")
+
+	return true, nil
 }
 
 // Use first test connectivity with curl and then to download the http and https root webpages
@@ -44,6 +68,11 @@ func downloadWebRootSource(ipAddress, protocol string) error {
 }
 
 func main() {
+	gracefulShutdown := make(chan os.Signal, 1)
+	signal.Notify(gracefulShutdown, syscall.SIGINT, syscall.SIGTERM)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
 	var ipAddress string
 	// var workingDirectory string
 
@@ -58,22 +87,39 @@ func main() {
 		os.Exit(-1)
 	}
 
-	// Ping host to get TTL data to detirmine OS type
-	pingHostThrice := exec.Command("ping", "-c 3", ipAddress)
-	_, err := pingHostThrice.StdoutPipe()
-	if err != nil {
-		panic(err)
-	}
-	if err := pingHostThrice.Start(); err != nil {
+	if ipAddress == "" || strings.Count(ipAddress, ".") != 3 {
+		err := fmt.Errorf("Invalid or no IP Address provided: %v", os.Args)
 		fmt.Fprintln(os.Stderr, "Error:", err)
-		fmt.Fprintf(os.Stdout, "Unable to execute `%s %s`\n", "ping -c 3 ", ipAddress)
-	}
-	if err := pingHostThrice.Wait(); err != nil {
-		fmt.Fprintln(os.Stderr, "Error:", err)
-		fmt.Fprintf(os.Stdout, "Unable to complete execution of `%s %s`\n", "ping -c 3 ", ipAddress)
+		os.Exit(-1)
 	}
 
-	time.Sleep(1 * time.Second)
-	fmt.Fprintf(os.Stdout, "Completed attempts to ping host")
+	connect, err := pingHost(ipAddress)
+	if connect != true || err != nil {
+		err := fmt.Errorf("Initial pinging of target failed, connectivity %v", connect)
+		fmt.Fprintln(os.Stderr, "Error:", err)
+		os.Exit(-1)
+	}
+
+	requestURL := fmt.Sprintf("http://%s", ipAddress)
+	req, err := http.NewRequestWithContext(ctx, "GET", requestURL, nil)
+	if err != nil {
+		fmt.Printf("Request creation failed: %v\n", err)
+		os.Exit(1)
+	}
+	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0")
+	req.Header.Set("User-Agentt", payloadBuilder.String())
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Printf("client: error making http request: %s\n", err)
+
+	}
+
+	fmt.Printf("client: got response!\n")
+	fmt.Printf("client: status code: %d\n", res.StatusCode)
+
+	<-gracefulShutdown
+	_, cancel = context.WithTimeout(context.Background(), 3*time.Second)
+	defer handleTermination(cancel)
 
 }
